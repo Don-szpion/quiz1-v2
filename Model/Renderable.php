7bdbd5638d1d0b6efce53d61c98fe80da9656902
Don-szpion <bookagemy4@gmail.com>
2017-02-04 00:44:46 +0200

    Add files via upload

---
+++ Model/Renderable.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: don-szpion
+ * Date: 01.02.17
+ * Time: 21:22
+ */
+interface Renderable
+{
+    public function render();
+}
---
+++ ProductCreator.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: don-szpion
+ * Date: 01.02.17
+ * Time: 20:01
+ */
+class ProductCreator
+{
+    protected static $_instance;
+    private function __construct()
+    {
+    }
+    private function __clone()
+    {
+    // TODO: Implement __clone() method.
+    }
+    public function getInctance(){
+     if (null === self::$_inctance){
+         self::$_inctance = new self();
+     }
+     return self::$_instance = new self();
+    }
+    public function make(string $prod, array $params)
+    {
+        $config = include "config.php";
+        if($prod === "iPhone" && $prod === $config){
+            $Phone = IPhoneProduct::__construct($params);
+            return $Phone;
+        } else if($prod === "iPad" && $prod === $config){
+            $Pad = IPadProduct::__construct($params);
+            return $Pad;
+        } else if ($prod === "MacBook" && $prod === $config){
+            $Mbook = MacBookProduct::__construct($params);
+            return $Mbook;
+        }
+    }
+
+}
